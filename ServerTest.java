35d28e7fafd0e9a2c89c83658bff7300cd5f3029
alex-cabral <60985583+alex-cabral@users.noreply.github.com>
2021-02-17 00:29:41 -0500

    Update ServerTest.java
    A few more tests with some edge cases

--- ServerTest.java
+++ ServerTest.java
@@ -1,27 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class ServerTest {
+public class ServerTest {
 	private Server server = new Server(5000);
-
+	
 	@Test 
 	// make sure a non-used username can be created
-	public void testAddUser() {
+	public void testAddUsername() {
 		server.addUsername("test");
 		assertEquals(true, server.checkUsername("test"));
 	}
 	
-	@Test 
-	// make sure an existing username can be removed
+	
+	@Test
+	// make sure a message can't get sent to a user who doesn't exist
+	public void testSendMessage() {
+		assertEquals(false, server.sendMessage("test", "notauser", ""));
+	}
+
+	@Test
+	// make sure a user without unread messages isn't flagged as having them,
+	// but that a user with unread messages is
+	public void testCheckForUnreadMessages() {
+		assertEquals(false, server.checkForUnreadMessages("test"));
+		assertEquals(true, server.checkForUnreadMessages("alice"));
+	}
+	
+
+	@Test
+	// make sure someone with unread messages gets a non null object back
+	public void testGetUnreadMessages() {
+		assertNotNull(server.getUnreadMessages("alice"));
+	}
+
+	@Test
+	// make sure removing a user removes them from the list
 	public void testRemoveUser() {
 		server.removeUser("test");
 		assertEquals(false, server.checkUsername("test"));
 	}
-	
-	
-
-	
-	
 
 }
